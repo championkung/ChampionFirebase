b191270d599d87803390e5f95d5b48d441f3a607
championkung <championkung@gmail.com>
2019-04-29 15:36:58 +0700

    Set Fonts & Images

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,10 +6,19 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget showAppName() {
+    return Text(
+      'Champion Flutter นะจ้ะ',
+      style: TextStyle(
+          fontFamily: 'Kanit',
+          fontSize: 24.0,
+          fontWeight: FontWeight.normal,
+          color: Colors.white),
+    );
+  }
 
-
-  Widget showAppName(){
-    return Text('Champion Flutter');
+  Widget showLogo() {
+    return Image.asset('assets/images/logo.png');
   }
 
   @override
@@ -17,10 +26,23 @@ class _AuthenState extends State<Authen> {
     // return Text('StatfulWidget');
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.only(top: 100.0),
-        alignment: Alignment(0, -1),
-        child: showAppName(),
-      ),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [Colors.teal[900], Colors.teal[400]], begin: Alignment(-1.0, -1.0))),
+          padding: EdgeInsets.only(top: 100.0),
+          alignment: Alignment(0, -1),
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: 128.0,
+                height: 128.0,
+                child: showLogo(),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 15.0),
+                child: showAppName(),
+              )
+            ],
+          )),
     );
   }
 }
